f981d2d1c1a2677e940c70c118a6d5f01d110235
josdem <joseluis.delacruz@gmail.com>
2020-03-26 19:34:40 -0400

    Starting new application webflux chatbot application

---
+++ src/main/java/com/jos/dem/chatbot/WebfluxChatbotApplication.java
@@ -0,0 +1,12 @@
+package com.jos.dem.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxChatbotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WebfluxChatbotApplication.class, args);
+  }
+}
