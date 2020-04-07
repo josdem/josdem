c4c68600c50a049a1d7f4e68b7bcf5fa46227d9a
josdem <joseluis.delacruz@gmail.com>
2020-04-06 20:54:45 -0400

    Adding basic JavaScript

---
+++ src/main/resources/static/basic.js
@@ -0,0 +1,18 @@
+var socket = new WebSocket('ws://localhost:8080/channel');
+
+socket.onopen = function (event) {
+    console.log('Connected to chatbot');
+    console.log(event);
+}
+
+socket.onmessage = function (event) {
+    console.log('Received: ' + event.data + '!');
+    var parseMessage = JSON.parse(event.data);
+    var ul = document.getElementById(
+        'comments: ' + parseMessage);
+    var li = document.createElement('li');
+    li.appendChild(
+        document.createTextNode(parseMessage));
+    ul.appendChild(li);
+}
+
