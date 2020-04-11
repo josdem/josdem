87d86de6d732d669b19fce1728ae070f35007a4a
josdem <joseluis.delacruz@gmail.com>
2020-04-11 19:47:14 -0400

    Receiving messages in basic html

--- src/main/resources/static/basic.js
+++ src/main/resources/static/basic.js
@@ -8,11 +8,6 @@ socket.onopen = function (event) {
 socket.onmessage = function (event) {
     console.log('Received: ' + event.data + '!');
     var parseMessage = JSON.parse(event.data);
-    var ul = document.getElementById(
-        'comments: ' + parseMessage);
-    var li = document.createElement('li');
-    li.appendChild(
-        document.createTextNode(parseMessage));
-    ul.appendChild(li);
+    console.log(parseMessage);
 }
 
